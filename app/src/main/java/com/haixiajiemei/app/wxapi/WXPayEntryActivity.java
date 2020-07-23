cdc7a7e884284c622d2179bed41a5121804020e2
ella <>
2020-07-23 11:11:26 +0800

    no message

---
+++ app/src/main/java/com/haixiajiemei/app/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,61 @@
+package com.haixiajiemei.app.wxapi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+    private static String TAG = "MicroMsg.WXEntryActivity";
+
+    private IWXAPI api;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        api = WXAPIFactory.createWXAPI(this, Constants.APP_ID, false);
+        api.handleIntent(getIntent(), this);
+
+        try {
+            Intent intent = getIntent();
+            api.handleIntent(intent, this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
+
+    @Override
+    public void onReq(BaseReq req) {
+//        switch (req.getType()) {
+//            case ConstantsAPI.COMMAND_GETMESSAGE_FROM_WX:
+//                goToGetMsg();
+//                break;
+//            case ConstantsAPI.COMMAND_SHOWMESSAGE_FROM_WX:
+//                goToShowMsg((ShowMessageFromWX.Req) req);
+//                break;
+//            default:
+//                break;
+//        }
+//        finish();
+    }
+
+    @Override
+    public void onResp(BaseResp resp) {
+        Log.d(TAG, "onPayFinish, errCode = " + resp.errCode);// 支付结果码
+    }
+}
